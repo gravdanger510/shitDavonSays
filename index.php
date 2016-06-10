84f58c8098ff80ef44740f3f0546f2503b8d0c33
gravdanger510 <gravdanger510@gmail.com>
2016-06-10 17:21:43 -0400

    Made random css local only

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<title></title>
-
+	<script src="html2canvas.js"></script>
+	<title>Davon once said:</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	<link rel="stylesheet" href="global.css">
 
 	<!--Load Random Stylesheet from css directory -->
 	<?php
@@ -12,20 +14,24 @@
 		foreach(glob($dir) as $file){
 			if(!is_dir($file)){
 				array_push($styles, $file);
-				
 			}
 		}
 		$rand =  rand(0, count($styles) - 1);
-		
+
 		echo '<link rel="stylesheet" type="text/css" href="' . $styles[$rand] . '">';
-
 	?>
-
 
 </head>
 <body>
 
 	<h1 id="davon"></h1>
+	<div id="icons">
+		<!-- <i class="fa fa-download" aria-hidden="true"></i> -->
+		<a href="https://github.com/gravdanger510/shitDavonSays" target="_blank">
+			<i class="fa fa-info-circle" aria-hidden="true"></i>
+		</a>
+	<!-- 	<i class="fa fa-upload" aria-hidden="true"></i> -->
+	</div>
 
 
 <script type="text/javascript">
@@ -47,6 +53,18 @@
 		console.log("running");
 		var rand = Math.floor(Math.random() * tweets.length - 1);
 		$("#davon").text(tweets[rand]);
+	},
+	downloadScreenshot = function(){
+		// var confirm = window.confirm("Download this Quote?");
+		var confirm = true;
+		if(confirm){
+			html2canvas(document.body, {
+			  onrendered: function(canvas) {
+			    var img = canvas.toDataURL("image/png");
+					document.write('<img src="'+img+'"/>');
+			  }
+			})
+		}
 	}
 	
 	
